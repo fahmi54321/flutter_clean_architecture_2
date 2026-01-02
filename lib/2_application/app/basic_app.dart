5fe436fe40a9bccd11a90535647763afd1f6f2ce
fahmiabdulaziz44 <fahmiabdulaziz44@gmail.com>
2026-01-02 10:56:30 +0700

    go router

--- lib/2_application/app/basic_app.dart
+++ lib/2_application/app/basic_app.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_clean_architecture_2/2_application/core/routes.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class BasicApp extends StatelessWidget {
   const BasicApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp.router(
+      title: 'ToDo App',
+      localizationsDelegates: [
+        ...GlobalMaterialLocalizations.delegates,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      themeMode: ThemeMode.system,
+      theme: ThemeData.from(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepOrange,
+          brightness: Brightness.light,
+        ),
+      ),
+      darkTheme: ThemeData.from(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+      ),
+      routerConfig: routes,
+    );
   }
 }

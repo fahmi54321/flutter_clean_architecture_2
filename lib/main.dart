44afab03b150625b5333e3d944dc87d1bfe0d47a
fahmiabdulaziz44 <fahmiabdulaziz44@gmail.com>
2026-01-08 13:06:58 +0700

    locale data source hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_clean_architecture_2/0_data/data_sources/local/memory_local_data_source.dart';
+import 'package:flutter_clean_architecture_2/0_data/data_sources/local/hive_local_data_source.dart';
 import 'package:flutter_clean_architecture_2/0_data/data_sources/repositories/todo_repository_local.dart';
 import 'package:flutter_clean_architecture_2/1_domain/repositories/todo_repository.dart';
 import 'package:flutter_clean_architecture_2/2_application/app/basic_app.dart';
 
-void main() {
+Future<void> main() async {
+  final localDataSource = HiveLocalDataSource();
+  await localDataSource.init();
+
   runApp(
     RepositoryProvider<ToDoRepository>(
       create: (context) =>
-          ToDoRepositoryLocal(localDataSource: MemoryLocalDataSource()),
+          ToDoRepositoryLocal(localDataSource: localDataSource),
       child: const BasicApp(),
     ),
   );
